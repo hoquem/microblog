5e0f97a843a7311aaf5cf194103e111973e2b566
Mahmudul Hoque <m.hoque@gmail.com>
2019-09-09 22:51:35 +0100

    password reset

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from logging.handlers import SMTPHandler, RotatingFileHandler
+from flask_mail import Mail
 import os
 
 
@@ -14,6 +15,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 
 if not app.debug:
