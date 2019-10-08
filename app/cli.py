b0421a0157ac73cbb4f467838010eb20f888086c
Mahmudul Hoque <m.hoque@gmail.com>
2019-10-08 22:27:38 +0100

    Feature/fulltextsearch (#10)
    * full text search
    
    * blueprints
    
    * full text search

--- app/cli.py
+++ app/cli.py
@@ -1,32 +1,34 @@
 import os
 import click
 
+
 def register(app):
     @app.cli.group()
     def translate():
-        """Translation and localisation commands."""
+        """Translation and localization commands."""
         pass
 
     @translate.command()
     @click.argument('lang')
     def init(lang):
-        """Initialise a new language."""
-        if os.system('pybabel extract -F babel.cfg -k _l messages.pot .'):
+        """Initialize a new language."""
+        if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
             raise RuntimeError('extract command failed')
-        if os.system('pybabel init -i message.pot -d app/translations -l ' + lang):
-            raise RuntimeError('extract command failed')
+        if os.system(
+                'pybabel init -i messages.pot -d app/translations -l ' + lang):
+            raise RuntimeError('init command failed')
         os.remove('messages.pot')
 
-    @translate.command() 
+    @translate.command()
     def update():
         """Update all languages."""
         if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
             raise RuntimeError('extract command failed')
         if os.system('pybabel update -i messages.pot -d app/translations'):
-            raise RuntimeError('update command failed') 
+            raise RuntimeError('update command failed')
         os.remove('messages.pot')
 
-    @translate.command() 
+    @translate.command()
     def compile():
         """Compile all languages."""
         if os.system('pybabel compile -d app/translations'):
--- app/translate.py
+++ app/translate.py
@@ -3,6 +3,7 @@ import requests
 from flask import current_app
 from flask_babel import _
 
+
 def translate(text, source_language, dest_language):
     if 'MS_TRANSLATOR_KEY' not in current_app.config or \
             not current_app.config['MS_TRANSLATOR_KEY']:
@@ -10,10 +11,9 @@ def translate(text, source_language, dest_language):
     auth = {
         'Ocp-Apim-Subscription-Key': current_app.config['MS_TRANSLATOR_KEY']}
     r = requests.get('https://api.microsofttranslator.com/v2/Ajax.svc'
-                    '/Translate?text={}&from={}&to={}'.format(
-                        text, source_language, dest_language), 
-                    headers=auth)
+                     '/Translate?text={}&from={}&to={}'.format(
+                         text, source_language, dest_language),
+                     headers=auth)
     if r.status_code != 200:
-        return _('Error: the translation service failed.')    
+        return _('Error: the translation service failed.')
     return json.loads(r.content.decode('utf-8-sig'))
-    
